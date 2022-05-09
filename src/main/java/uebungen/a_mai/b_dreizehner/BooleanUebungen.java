b7301cc229fe22558df4f4cb104242a9bf4533c1
Johannes von Zmuda <johannesvonzmuda@gmail.com>
2022-05-09 18:42:41 +0200

    Uebungen fuer boolean

---
+++ src/main/java/uebungen/a_mai/b_dreizehner/BooleanUebungen.java
@@ -0,0 +1,81 @@
+package uebungen.a_mai.b_dreizehner;
+
+public class BooleanUebungen {
+
+    public static void wahr(){
+        // FIXME Hier soll der Wert true in die Variable wahr gespeichert werden.
+        boolean wahr = false;
+
+        System.out.println(wahr);
+    }
+
+    public static void falsch(){
+        // FIXME Hier soll der Wert false in die Variable falsch gespeichert werden.
+        boolean falsch = true;
+
+        System.out.println(falsch);
+    }
+
+    public static void istWahr(boolean a){
+        // FIXME Hier soll geprüft werden, ob der Wert a true ist.
+        //  Wenn der Wert in a true ist, soll in die Variable istWahr den Wert true haben.
+        //  Wenn der Wert in a false ist, soll die Variable istWahr den Wert false haben.
+        boolean wahr = false;
+
+        System.out.println(wahr);
+    }
+
+    public static void istFalsch(boolean a){
+        // FIXME Hier soll geprüft werden, ob der Wert a false ist.
+        //  Wenn der Wert in a true ist, soll in die Variable istWahr den Wert true haben.
+        //  Wenn der Wert in a false ist, soll die Variable istWahr den Wert false haben.
+        boolean falsch = false;
+
+        System.out.println(falsch);
+    }
+
+    public static void istGleich(boolean a, boolean b) {
+        // FIXME Hier soll geprüft werden, ob der Wert in a und b gleich ist.
+        // Wenn der Wert gleich ist, soll die Variable istGleich den Wert true haben.
+        // Ansonsten soll die Variable istGleich false sein.
+        boolean istGleich = false;
+
+        System.out.println(istGleich);
+    }
+
+    public static void istUngleich(boolean a, boolean b) {
+        // FIXME Hier soll geprüft werden, ob der Wert in a und b ungleich ist.
+        // Wenn der Wert ungleich ist, soll die Variable istUngleich den Wert true haben.
+        // Ansonsten soll die Variable istUngleich false sein.
+        boolean istUngleich = false;
+
+        System.out.println(istUngleich);
+    }
+
+    public static void oder(boolean a, boolean b) {
+        // FIXME Hier soll geprüft werden, ob der Wert in a oder b true ist.
+        // Wenn a oder b wahr ist, soll die Variable oder den Wert true haben.
+        // Ansonsten soll die Variable oder false sein.
+        boolean oder = false;
+
+        System.out.println(oder);
+    }
+
+    public static void und(boolean a, boolean b) {
+        // FIXME Hier soll geprüft werden, ob der Wert in a und b true ist.
+        // Sind beide Werte wahr, soll die Variable und den Wert true haben.
+        // Ansonsten soll die Variable und false sein.
+        boolean und = false;
+
+        System.out.println(und);
+    }
+
+    public static void nicht(boolean a) {
+        // FIXME Hier soll das Gegenteil von Wert a in der Variable nicht gespeichert werden.
+        // Ist der Wert in a wahr, soll die Variable nicht den Wert false haben.
+        // Ist der Wert in a falsch, soll die Variable nicht den Wert true haben.
+        boolean nicht = false;
+
+        System.out.println(nicht);
+    }
+}
---
+++ src/test/java/uebungen/a_mai/b_dreizehner/BooleanUebungenTest.java
@@ -0,0 +1,119 @@
+package uebungen.a_mai.b_dreizehner;
+
+import org.junit.jupiter.api.Test;
+import uebungen.a_mai.SystemOutMockingTest;
+
+class BooleanUebungenTest extends SystemOutMockingTest {
+
+    @Test
+    void wahr() {
+        BooleanUebungen.wahr();
+
+        assertSystemOutEquals("true");
+    }
+
+    @Test
+    void falsch() {
+        BooleanUebungen.falsch();
+
+        assertSystemOutEquals("false");
+    }
+
+    @Test
+    void istWahrIstTrue() {
+        BooleanUebungen.istWahr(true);
+
+        assertSystemOutEquals("true");
+    }
+
+    @Test
+    void istWahrIstFalse() {
+        BooleanUebungen.istWahr(false);
+
+        assertSystemOutEquals("false");
+    }
+
+    @Test
+    void istFalschIstTrue() {
+        BooleanUebungen.istFalsch(true);
+
+        assertSystemOutEquals("false");
+    }
+
+    @Test
+    void istFalschIstFalse() {
+        BooleanUebungen.istFalsch(false);
+
+        assertSystemOutEquals("true");
+    }
+
+    @Test
+    void istGleichIstFalse() {
+        BooleanUebungen.istGleich(true, false);
+
+        assertSystemOutEquals("false");
+    }
+
+    @Test
+    void istGleichIstTrue1() {
+        BooleanUebungen.istGleich(true, true);
+
+        assertSystemOutEquals("true");
+    }
+
+    @Test
+    void istGleichIstTrue2() {
+        BooleanUebungen.istGleich(false, false);
+
+        assertSystemOutEquals("true");
+    }
+
+    @Test
+    void istUngleichIstWahr() {
+        BooleanUebungen.istUngleich(true, false);
+
+        assertSystemOutEquals("true");
+    }
+
+    @Test
+    void istUngleichIstFalsch1() {
+        BooleanUebungen.istUngleich(false, false);
+
+        assertSystemOutEquals("false");
+    }
+
+    @Test
+    void istUngleichIstFalsch2() {
+        BooleanUebungen.istUngleich(true, true);
+
+        assertSystemOutEquals("false");
+    }
+
+    @Test
+    void oderIstTrue() {
+        BooleanUebungen.oder(true, false);
+
+        assertSystemOutEquals("true");
+    }
+
+    @Test
+    void oderIstFalse() {
+        BooleanUebungen.oder(false, false);
+
+        assertSystemOutEquals("false");
+    }
+
+    @Test
+    void undIstWahr() {
+        BooleanUebungen.und(true, true);
+
+        assertSystemOutEquals("true");
+    }
+
+    @Test
+    void undIstFalse() {
+        BooleanUebungen.und(false, true);
+
+        assertSystemOutEquals("false");
+    }
+}
