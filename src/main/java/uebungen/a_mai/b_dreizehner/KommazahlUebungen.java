c04b1ee4340b4f2a03e687f40d6ddbab197065bb
Johannes von Zmuda <johannesvonzmuda@gmail.com>
2022-05-09 18:08:21 +0200

    Uebungen fuer Kommazahlen

--- src/main/java/uebungen/a_mai/b_dreizehner/KommazahlUebungen.java
+++ src/main/java/uebungen/a_mai/b_dreizehner/KommazahlUebungen.java
@@ -3,31 +3,52 @@ package uebungen.a_mai.b_dreizehner;
 public class KommazahlUebungen {
 
     public static void pi() {
-        // FIXME Hier soll die Zahl PI (ca. 3,14) mit 2 Nachkommastellen in die Variable pi geschrieben werden.
+        // FIXME Hier soll die Zahl PI (ca. 3,14) mit 2 Nachkommastellen Genauigkeit in die Variable pi geschrieben werden.
         double pi = 0d;
 
         System.out.println("Pi entspricht circa " + pi);
     }
 
-    public static void addiere(double a, double b) {
-        // FIXME Hier muss die Summe aus zwei Ganzzahlen berechnet werden.
+    public static void addierePi(double a) {
+        // FIXME Hier soll die Summe aus der Variable a und dem Wert 3,14 berechnet werden.
         double summe = 0d;
 
-        System.out.println("Summe von a=" + a + " + b=" + b + " ist: " + summe);
+        System.out.println(a + " + 3.14 = " + summe);
+    }
+
+    public static void addiere(double a, double b) {
+        // FIXME Hier soll die Summe aus zwei Ganzzahlen berechnet werden.
+        double summe = 0d;
+
+        System.out.println(a + " + " + b + " = " + summe);
     }
 
     public static void substrahiere(double a, double b) {
-        // FIXME Hier muss Zahl b von Zahl a abgezogen werden.
+        // FIXME Hier soll Zahl b von Zahl a abgezogen werden.
         double differenz = 0d;
 
-        System.out.println("Differenz von a=" + a + " und b=" + b + " ist: " + differenz);
+        System.out.println(a + " - " + b + " = " + differenz);
+    }
+
+    public static void multipliziere(double a, double b) {
+        // FIXME Hier soll Zahl a mit Zahl b multipliziert werden.
+        double differenz = 0d;
+
+        System.out.println(a + " * " + b + " = " + differenz);
+    }
+
+    public static void dividiere(double a, double b) {
+        // FIXME Hier soll Zahl a durch Zahl b geteilt werden.
+        double differenz = 0d;
+
+        System.out.println(a + " / " + b + " = " + differenz);
     }
 
     public static void durchschnitt(double a, double b, double c) {
-        // FIXME Hier soll der Durschnitt von den Zahlen a, b und c berechnet werden.
+        // FIXME Hier soll der Durchschnitt von den Zahlen a, b und c berechnet werden.
         double durchschnitt = 0d;
 
-        System.out.println("Durchschnitt von a=" + a + ", b=" + b + " und c=" + c + " ist: " + durchschnitt);
+        System.out.println("Durchschnitt von " + a + ", " + b + " und " + c + " ist " + durchschnitt);
     }
 
 }
--- src/test/java/uebungen/a_mai/b_dreizehner/KommazahlUebungenTest.java
+++ src/test/java/uebungen/a_mai/b_dreizehner/KommazahlUebungenTest.java
@@ -13,23 +13,44 @@ class KommazahlUebungenTest extends SystemOutMockingTest {
     }
 
     @Test
+    void addierePi() {
+        KommazahlUebungen.addierePi(1.23d);
+
+        assertSystemOutEquals("1.23 + 3.14 = 5.79");
+    }
+
+    @Test
     void addiere() {
         KommazahlUebungen.addiere(1.23d, 4.56d);
 
-        assertSystemOutEquals("Summe von a=1.23 + b=4.56 ist: 5.79");
+        assertSystemOutEquals("1.23 + 4.56 = 5.79");
     }
 
     @Test
     void substrahiere() {
         KommazahlUebungen.substrahiere(4.56d, 1.23d);
 
-        assertSystemOutEquals("Differenz von a=4.56 und b=1.23 ist: 3,33");
+        assertSystemOutEquals("4.56 - 1.23 = 3,33");
+    }
+
+    @Test
+    void multipliziere() {
+        KommazahlUebungen.multipliziere(4.56d, 1.23d);
+
+        assertSystemOutEquals("4.56 - 1.23 = 3,33");
+    }
+
+    @Test
+    void dividiere() {
+        KommazahlUebungen.dividiere(4.56d, 1.23d);
+
+        assertSystemOutEquals("4.56 - 1.23 = 3,33");
     }
 
     @Test
     void durchschnitt() {
         KommazahlUebungen.durchschnitt(1.23d, 4.56d, 7.98d);
 
-        assertSystemOutEquals("Durchschnitt von a=1.23, b=4.56 und c=7.98 ist: 4,56");
+        assertSystemOutEquals("Durchschnitt von 1.23, 4.56 7.98 ist 4,56");
     }
 }
