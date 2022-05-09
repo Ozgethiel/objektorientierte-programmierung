48325b202f59c9c92e6d3aaa64179502354200e7
Johannes von Zmuda <johannesvonzmuda@gmail.com>
2022-05-09 20:07:50 +0200

    Fortgeschrittene Uebungen

---
+++ src/main/java/uebungen/a_mai/b_dreizehner/FortgeschritteneUebungen.java
@@ -0,0 +1,30 @@
+package uebungen.a_mai.b_dreizehner;
+
+public class FortgeschritteneUebungen {
+
+    public static void aGroesserAlsBUndKleinerAlsC(int a, int b, int c) {
+        // FIXME: Es soll geprüft werden, ob a kleiner als b und größer als c ist.
+        //  Wenn das Ergebnis wahr ist, soll true auf der Konsole ausgegeben werden.
+        //  Ansonsten soll false auf der Konsole ausgegeben werden.
+    }
+
+    public static void ceasarVerschluesselung(char a) {
+        // FIXME: Ceasar soll damals eine Verschlüsselung benutzt haben, in der jeder Buchstabe durch seinen 5. Nachfolger im Alphabet ersetzt wird.
+        //  Hier soll der Buchstabe in Variable a mithilfe dieser Verschlüsselung verschlüsselt werden.
+        //  Beispiel: a wird zu f, b wird zu g
+        //  VORSICHT! Was passiert mit z? Bei z geht es wieder von vorne los. Aber wie geht das?
+        //  Gib den verschlüsselten Buchstaben auf der Konsole aus. Es werden nur kleine Buchstaben verwendet.
+    }
+
+    public static void restwertBeiKommazahlen(double a, double b) {
+        // FIXME: Der Restwert bei der Division der Variablen a und b soll berechnet werden.
+        //  Ist das überhaupt möglich? Mit welchen Einschränkungen ginge das?
+        //  Gib den ganzzahligen Restwert auf der Konsole aus.
+    }
+
+    public static void abrunden(double a) {
+        // FIXME: Die Zahl a soll ab der 3. Nachkommastelle abgerundet werden.
+        //  Beispiel: Aus PI (3.14159265359...) würde 3.141
+    }
+
+}
---
+++ src/test/java/uebungen/a_mai/b_dreizehner/FortgeschritteneUebungenTest.java
@@ -0,0 +1,65 @@
+package uebungen.a_mai.b_dreizehner;
+
+import org.junit.jupiter.api.Test;
+import uebungen.a_mai.SystemOutMockingTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FortgeschritteneUebungenTest extends SystemOutMockingTest {
+
+    @Test
+    void aGroesserAlsBUndKleinerAlsCTrue() {
+        FortgeschritteneUebungen.aGroesserAlsBUndKleinerAlsC(2, 1, 3);
+
+        assertSystemOutEquals("true");
+    }
+
+    @Test
+    void aGroesserAlsBUndKleinerAlsCFalse1() {
+        FortgeschritteneUebungen.aGroesserAlsBUndKleinerAlsC(1, 2, 3);
+
+        assertSystemOutEquals("false");
+    }
+
+    @Test
+    void aGroesserAlsBUndKleinerAlsCFalse2() {
+        FortgeschritteneUebungen.aGroesserAlsBUndKleinerAlsC(2, 1, 1);
+
+        assertSystemOutEquals("false");
+    }
+
+    @Test
+    void ceasarVerschluesselungA() {
+        FortgeschritteneUebungen.ceasarVerschluesselung('m');
+
+        assertSystemOutEquals("r");
+    }
+
+    @Test
+    void ceasarVerschluesselungZ() {
+        FortgeschritteneUebungen.ceasarVerschluesselung('z');
+
+        assertSystemOutEquals("d");
+    }
+
+    @Test
+    void restwertBeiKommazahlenOhneRestwert() {
+        FortgeschritteneUebungen.restwertBeiKommazahlen(10.98d, 5.43d);
+
+        assertSystemOutEquals("0");
+    }
+
+    @Test
+    void restwertBeiKommazahlenMitRestwert() {
+        FortgeschritteneUebungen.restwertBeiKommazahlen(10.98d, 4.43d);
+
+        assertSystemOutEquals("2");
+    }
+
+    @Test
+    void abrunden() {
+        FortgeschritteneUebungen.abrunden(Math.PI);
+
+        assertSystemOutEquals("3.141");
+    }
+}
