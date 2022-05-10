041d0b94eee6d8da76824b26804a9220bf565c2c
Johannes von Zmuda <johannesvonzmuda@gmail.com>
2022-05-10 10:54:25 +0200

    Parameter umbenannt

--- src/main/java/uebungen/a_mai/b_dreizehner/CharUebungen.java
+++ src/main/java/uebungen/a_mai/b_dreizehner/CharUebungen.java
@@ -10,8 +10,8 @@ public class CharUebungen {
         // FIXME Hier soll das Sonderzeichen Tab auf der Konsole ausgegeben werden.
     }
 
-    public static void grossbuchstabe(char a) {
-        // FIXME Hier soll das Zeichen in Variable a als Großbuchstabe auf der Konsole ausgegeben werden.
+    public static void grossbuchstabe(char kleinbuchstabe) {
+        // FIXME Hier soll das Zeichen in Variable kleinbuchstabe als Großbuchstabe auf der Konsole ausgegeben werden.
         //  Es werden nur Kleinbuchstaben zwischen a und z an diese Methode übergeben.
     }
 }
