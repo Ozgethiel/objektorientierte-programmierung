3af203809277192bd93a52daf556e2864644524b
Akademie <othiel@uni-bremen.de>
2022-05-12 11:15:08 +0200

    Mein Namen ausgeben

--- src/main/java/uebungen/a_mai/a_helloworld/HelloWorld.java
+++ src/main/java/uebungen/a_mai/a_helloworld/HelloWorld.java
@@ -2,6 +2,6 @@ package uebungen.a_mai.a_helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Ozge!");
     }
 }
