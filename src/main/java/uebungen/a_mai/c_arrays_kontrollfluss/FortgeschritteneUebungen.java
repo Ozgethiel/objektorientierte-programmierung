c50bbd911f16a70438cbefd747bae83f664fa03b
Akademie <othiel@uni-bremen.de>
2022-05-20 09:12:22 +0200

    Aufgaben Özge

--- src/main/java/uebungen/a_mai/c_arrays_kontrollfluss/FortgeschritteneUebungen.java
+++ src/main/java/uebungen/a_mai/c_arrays_kontrollfluss/FortgeschritteneUebungen.java
@@ -1,11 +1,23 @@
 package uebungen.a_mai.c_arrays_kontrollfluss;
 
+import java.util.Arrays;
+
 public class FortgeschritteneUebungen {
 
     public static void umkehren(int[] zahlen) {
         // FIXME: Diese Funktion soll die Zahlen im Array zahlen in umgekehrter Reihenfolge in das selbe Array speichern.
         //  Beispiel: Aus 1,2,3 wird 3,2,1
+
+
+        for (int i = 0; i < zahlen.length / 2; i++) {
+
+            int swap = zahlen[i];
+            zahlen[i] = zahlen[zahlen.length - i - 1];
+            zahlen[zahlen.length - i - 1] = swap;
+        }
+
     }
+
 
     public static char[][] dame() {
         // FIXME: Diese Methode soll ein 2-dimensionales char-Array erzeugen. Das Array soll die Start-Aufstellung in einem Dame-Feld repräsentieren.
@@ -17,7 +29,23 @@ public class FortgeschritteneUebungen {
         //  Wenn auf dem Feld ein weißer Stein liegt, soll der Eintrag ein großes W enthalten.
         //  Der erste Index identifiziert die Zeilen, der zweite Index die Spalten.
         //  Die weißen Steine sind in den ersten 3 Zeilen aufgestellt, die schwarzen Steine sind den letzten 3 Zeilen aufgestellt.
-        return null;
+        char s = 'S';
+        char w = 'W';
+        char x = ' ';
+        char[] wline1 = {w, x, w, x, w, x, w, x};
+        char[] wline2 = {x, w, x, w, x, w, x, w};
+        char[] wline3 = {w, x, w, x, w, x, w, x};
+        char[] wline4 = {x, x, x, x, x, x, x, x};
+        char[] line1 = {x, x, x, x, x, x, x, x};
+        char[] line2 = {x, s, x, s, x, s, x, s};
+        char[] line3 = {s, x, s, x, s, x, s, x};
+        char[] line4 = {x, s, x, s, x, s, x, s};
+
+
+        char[][] a = {wline1, wline2, wline3, wline4, line1, line2, line3, line4,};
+        System.out.println(a);
+
+        return a;
     }
 
     public static boolean[] siebDesEratosthenes(int max) {
@@ -39,7 +67,21 @@ public class FortgeschritteneUebungen {
     public static void sortieren(int[] zahlen) {
         // FIXME: Die Zahlen im Array zahlen sind durcheinander geraten. Bitte sortiert die Zahlen aufsteigend nach ihrer Größe.
         //  Beispiel: aus 3,1,2 soll 1,2,3 werden.
+
+        int len = zahlen.length;
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = 0; j < len; j++) {
+                if (zahlen[i] < zahlen[j]) {
+                    int h = zahlen[i];
+                    zahlen[i] = zahlen[j];
+                    zahlen[j] = h;
+
+                }
+            }
+        }
+
     }
+
 
     public static void roemischeZahlen(int dezimal) {
         // FIXME: Die Zahl dezimal soll als römische Zahl ausgegeben werden.
@@ -56,5 +98,59 @@ public class FortgeschritteneUebungen {
         //  Beispiele: 4 = IV, 90 = XM
         //  Die einzelnen Buchstaben können mit System.out.print(char); ausgegeben werden. Bitte gebt nach eurer Ausgabe noch ein Leerzeichen mit
         //  System.out.println(); aus.
+        String roemisch = "";
+
+        int ms = dezimal / 1000;
+        for (int i = 0; i < ms; i++) {
+            roemisch = roemisch + "M";
+        }
+        dezimal = dezimal - ms * 1000;
+
+        int ds = dezimal / 500;
+        for (int i = 0; i < ds; i++) {
+            roemisch = roemisch + "D";
+        }
+        dezimal = dezimal - ds * 500;
+
+        int cs = dezimal / 100;
+        if (cs == 4) {
+            roemisch = roemisch + "CD";
+        } else {
+            for (int i = 0; i < cs; i++) {
+                roemisch = roemisch + "C";
+            }
+        }
+        //System.out.println(dezimal);
+        dezimal = dezimal - cs * 100;
+
+        int ls = dezimal / 50;
+        for (int i = 0; i < ls; i++) {
+            roemisch = roemisch + "L";
+        }
+        dezimal = dezimal - ls * 50;
+
+        int xs = dezimal / 10;
+        for (int i = 0; i < xs; i++) {
+            roemisch = roemisch + "X";
+        }
+        dezimal = dezimal - xs * 10;
+
+        int vs = dezimal / 5;
+        for (int i = 0; i < vs; i++) {
+            roemisch = roemisch + "V";
+        }
+        dezimal = dezimal - vs * 5;
+
+        int is = dezimal / 1;
+        if (is == 4) {
+            roemisch = roemisch + "IV";
+        } else {
+            for (int i = 0; i < is; i++) {
+                roemisch = roemisch + "I";
+            }
+        }
+            System.out.println(roemisch);
+            // System.out.println(dezimal);
+        }
     }
-}
+
