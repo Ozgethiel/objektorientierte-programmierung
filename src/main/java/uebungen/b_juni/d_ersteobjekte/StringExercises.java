b615434f6e462cee4744d3c72bb5f5e7cfadcf56
Johannes von Zmuda <johannesvonzmuda@gmail.com>
2022-06-01 21:13:26 +0200

    String Uebungen

---
+++ src/main/java/uebungen/b_juni/d_ersteobjekte/StringExercises.java
@@ -0,0 +1,22 @@
+package uebungen.b_juni.d_ersteobjekte;
+
+public class StringExercises {
+
+    public static char charAtPosition(String text, int position) {
+        // FIXME: Hier soll das Zeichen an der Stelle position aus dem String text zurückgegeben werden. Der String enthält mindestens
+        //  position Zeichen.
+        return ' ';
+    }
+
+    public static String substringBefore(String text, String word) {
+        // FIXME: Hier soll die Zeichenkette text an der Stelle abgetrennt werden, an der das erste Mal der String word auftaucht. Das Wort word existiert
+        //  immer in dem String text.
+        return null;
+    }
+
+    public static String optimist(String text) {
+        // FIXME: Im String text soll das Wort "halbleer" durch das Wort "halbvoll" ersetzt werden.
+        return null;
+    }
+
+}
