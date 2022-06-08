f31dc376b2bfe027a2af16a6e10da27961d2b473
Johannes von Zmuda <johannesvonzmuda@gmail.com>
2022-06-08 08:32:12 +0200

    Loesung Muenzwurf entfernt

--- src/main/java/uebungen/b_juni/j_cointoss/Main.java
+++ src/main/java/uebungen/b_juni/j_cointoss/Main.java
@@ -19,24 +19,5 @@ public class Main {
         //  toss Methode der Coin Instanz aufgerufen. Dann wird der Sieger ermittelt. Der Verlierer zahlt dem Gewinner 1 Euro
         //  mithilfe der giveMoney Methode. Geben Sie nach jedem Spiel den Sieger aus und wie viel Geld jeder Spieler noch hat.
         //  Fortgeschritten: Geht das auch mit mehr als nur 2 Spielern?
-        Player player1 = new Player(CoinSide.HEADS, 100, "Luke Skywalker");
-        Player player2 = new Player(CoinSide.TAILS, 100, "Leia Skywalker");
-        Coin coin = new Coin();
-        System.out.println(player1.getName() + " bets for " + player1.getBet().name());
-        System.out.println(player2.getName() + " bets for " + player2.getBet().name());
-
-        for (int i = 0; i < 10; i++) {
-            CoinSide coinSide = coin.toss();
-            System.out.println("Coin toss resulted in " + coinSide.name());
-            if (coinSide == player1.getBet()) {
-                System.out.println(player1.getName() + " wins");
-                player2.giveMoney(player1);
-            } else {
-                System.out.println(player2.getName() + " wins");
-                player1.giveMoney(player2);
-            }
-            System.out.println(player1.getName() + ": " + player1.getMoney()
-                    + " " + player2.getName() + ": " + player2.getMoney());
-        }
     }
 }
