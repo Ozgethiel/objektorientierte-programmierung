a7ec55ee792b2e9ada10b76ed121783cff8cdc96
Johannes von Zmuda <johannesvonzmuda@gmail.com>
2022-07-07 01:14:27 +0200

    Uebung verbessert

--- src/main/java/uebungen/c_juli/c_pairs/Translator.java
+++ src/main/java/uebungen/c_juli/c_pairs/Translator.java
@@ -1,5 +1,8 @@
 package uebungen.c_juli.c_pairs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Translator {
 
     public static void main(String[] args) {
@@ -13,49 +16,53 @@ public class Translator {
         //      die die entsprechenden Werte aus key und value zurückgibt.
         //   5. Erstellt eine Klasse Map. Auch die Klasse Map hat die
         //      beiden Typ-Parameter K und V.
-        //   6. Die Klasse Map enthält ein Array vom Typ Pair.
-        //      Jedes Pair in dem Array besitzt einen key vom Typ K
+        //   6. Die Klasse Map enthält eine List vom Typ Pair.
+        //      Jedes Pair in der List besitzt einen key vom Typ K
         //      und einen value vom Typ V.
-        //   7. Die Klasse Map wird mit einem Konstruktor mit einem
-        //      int-Parameter erstellt, der angibt, wie groß das Array
-        //      von Pairs sein soll.
+        //   7. Die Klasse Map wird mit einem Konstruktor in dem Die List
+        //      vom Typ Pair mit einer ArrayList initialisiert wird.
         //   8. Außerdem hat die Klasse Map eine Methode put, die einen
         //      Wert K und einen Wert V entgegennimmt. Die Methode erzeugt
-        //      ein Pair von diesen beiden Werten und fügt es in das Array
-        //      hinzu. Die Methode put hat keinen Rückgabetyp (void).
+        //      ein Pair von diesen beiden Werten und fügt es in die List
+        //      Pair hinzu. Die Methode put hat keinen Rückgabetyp (void).
         //   9. Desweiteren hat die Klasse Map eine Methode get, die
         //      einen Parameter vom Typ K entgegennimmt. Sie sucht das
-        //      Pair mit dem gleichen key aus ihrem Array und gibt den
-        //      entsprechenden Value zurück.
+        //      Pair mit dem gleichen key aus ihrer List und gibt den
+        //      entsprechenden Value zurück. Wenn kein Eintrag gefunden
+        //      wird, wird eine IllegalArgumentException geworfen.
         //  10. Erstellt in dieser Main Klasse eine Map germanNumbers,
-        //      die 10 Pairs mit dem Key-Typ Integer und Value-Typ String
+        //      die Pair's mit dem Key-Typ Integer und Value-Typ String
         //      fassen kann.
         //  11. Fügt in die Map 10 Werte ein:
+        //      0 - "Null"
         //      1 - "Eins"
         //      2 - "Zwei"
         //      3 - "Drei"
         //      ...
         //      9 - "Neun"
-        //  12. Schreibt in dieser Klasse Pairs eine static Methode
-        //      translate mit 2 Typ-Parametern, K und V. Die die einen
-        //      Parameter vom Typ K-Array und einen Parameter vom Typ
-        //      Map mit dem Key-Typ K und Value-Typ V annimmt und ein
-        //      Array vom Typ V zurückgibt. Die Methode übersetzt jeden
-        //      Key in dem Array in den entsprechenden Value, die in
-        //      dem Map-Parameter angegeben ist.
+        //  12. Schreibt in dieser Klasse Translator eine static Methode
+        //      translate mit 2 Typ-Parametern, K und V. Die Methode
+        //      akzeptiert 2 Parametern:
+        //      einen Parameter vom Typ List mit Einträgen vom Typ K
+        //      und einen Parameter vom Typ Map mit dem Key-Typ K und
+        //      Value-Typ V.
+        //      Die Methode gibt eine List mit Einträgen vom Typ V
+        //      zurückgibt. Die Methode übersetzt jeden Key in der List
+        //      mit den entsprechenden Value, der in dem Map-Parameter
+        //      angegeben ist.
         //  13. Benutzt die Methode mit der Variable germanNumbers
         //      um eine beliebige Zahlenfolge in die entsprechenden
         //      natürlichsprachlichen Zahlen zu übersetzen
         //      (z.B. eure Matrikelnummer oder eure Postleitzahl).
-        //      Also aus einem int-Array
+        //      Also aus einem int-List
         //      {2,8,2,0,7}
-        //      wird das String-Array
+        //      wird eine String-List
         //      {"Zwei", "Acht", "Zwei", "Null", "Sieben"}
-        //      Gebt das Array auf der Konsole aus.
+        //      Gebt die String-List auf der Konsole aus.
         //  14. Erstellt eine zweite Map vom gleichen Typ wie germanNumbers,
         //      aber diesmal speichert die Zahlen in der Sprache eurer Wahl
         //      ein.
-        //      Gebt das Array auf der Konsole aus.
+        //      Gebt die List auf der Konsole aus.
         //  15. Nutzt die Methode translate um eure Zeichenfolge in der
         //      anderen Sprache auszugeben.
         //  16. Schreibt eine Methode flip in der Klasse Map, die eine Map
@@ -65,9 +72,9 @@ public class Translator {
         //      Die Werte sollen beibehalten bleiben.
         //  17. Benutzt die translate-Methode um die natürlich-sprachlichen
         //      Zahlen wieder in die Zahlen zu übersetzen.
-        //      Also aus einem String-Array
+        //      Also aus einer String-List
         //      {"Zwei", "Acht", "Zwei", "Null", "Sieben"}
-        //      wird wieder das int-Array
+        //      wird wieder die int-List
         //      {2,8,2,0,7}
         //  18. Erstellt eine Map mit Key-Typ String und Value-Typ String.
         //      Speichert dieses Mal die natürlich-sprachlichen Zahlen auf
@@ -79,5 +86,7 @@ public class Translator {
         //      ...
         //  19. Benutzt die Map um eine Zahl in einer Sprache in eine andere
         //      zu übersetzen.
+        //  Hinweis: Mit List.of() könnt ihr schnell und einfach Listen mit
+        //  festen Werten erzeugen.
     }
 }
